411ef9f12ec0571b7b78748a46138b963f3e0176
Frederic Lepied <flepied@gmail.com>
2017-12-22 05:15:01 +0100

    balances.py: compute total value in BTC

--- balances.py
+++ balances.py
@@ -5,9 +5,21 @@ from bittrex_exchange import BittrexExchange
 exch = BittrexExchange(True)
 
 balances = exch.get_balances()
+total = 0
 
 for b in balances:
     bb = b['Balance']
     if bb['Balance'] > 0 or bb['Available'] > 0:
-        print('[%s] balance: %2.8f available %2.8f' %
-              (bb['Currency'], bb['Balance'], bb['Available']))
+        btc = b.get('BitcoinMarket', None)
+        if btc:
+            val = btc['Last'] * bb['Balance']
+            print('[%s] balance: %.8f available %.8f value %.8f BTC' %
+                  (bb['Currency'], bb['Balance'], bb['Available'], val))
+            total += val
+        else:
+            print('[%s] balance: %.8f available %.8f' %
+                  (bb['Currency'], bb['Balance'], bb['Available']))
+            if bb['Currency'] == 'BTC':
+                total += bb['Balance']
+print()
+print('Total %.8f BTC' % total)
